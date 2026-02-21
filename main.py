8e25ba22577886baebab35cb29881e21af96317d
mindsetforge20268-netizen <mindsetforge20268@gmail.com>
2026-02-21 20:39:34 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+
+print("--- Price Tracker Starting ---")
+
+# This keeps the container running so it doesn't just exit immediately
+while True:
+    print("Checking prices... (everything looks good!)")
+    time.sleep(60)
